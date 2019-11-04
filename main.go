dfb6fa98caac9a7f52e03db55aece135aed942a5
richmanfx <richmanfx@yandex.ru>
2019-11-04 15:08:13 +0300

    Оставлена только сериализация для новой структуры

--- main.go
+++ main.go
@@ -5,30 +5,14 @@
 package main
 
 import (
-	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
 )
 
-type Config1 struct {
-	// Общие параметры приложения
-	LogLevel       string `yaml:"LogLevel"`
-	ThreadsCount   string `yaml:"ThreadsCount"`
-	DateTimeFormat string `yaml:"DateTimeFormat"`
-
-	// Параметры каждого из Бекапов
-	From        string `yaml:"From"`
-	To          string `yaml:"To"`
-	OutFileName string `yaml:"OutFileName"`
-	Crypt       string `yaml:"Crypt"`
-	DateTime    string `yaml:"DateTime"`
-	Ssh         string `yaml:"Ssh"`
-}
-
+// Параметры каждого из Бекапов
 type Backup struct {
-	// Параметры каждого из Бекапов
 	From        string `yaml:"From"`
 	To          string `yaml:"To"`
 	OutFileName string `yaml:"OutFileName"`
@@ -37,11 +21,12 @@ type Backup struct {
 	Ssh         string `yaml:"Ssh"`
 }
 
-type Config2 struct {
-	LogLevel       string `yaml:"LogLevel"`
-	ThreadsCount   int    `yaml:"ThreadsCount"`
-	DateTimeFormat string `yaml:"DateTimeFormat"`
-	Backups        []Backup
+// Конфиг полностью
+type Config struct {
+	LogLevel       string   `yaml:"LogLevel"`
+	ThreadsCount   int      `yaml:"ThreadsCount"`
+	DateTimeFormat string   `yaml:"DateTimeFormat"`
+	Backups        []Backup `yaml:"Backups"`
 }
 
 func main() {
@@ -49,79 +34,52 @@ func main() {
 	const configFileName = "gobackuper.yaml"
 
 	var (
-		//debugLevel = "DEBUG" // "DEBUG", "INFO"
-		//configs    []Config1
-		config3 Config2
+		config Config
 	)
 
 	// Выставить параметры логирования
 	SetLog(log.DebugLevel)
 
 	// Получить параметры из конфигурационного файла
-	//getConfigParameters(configFileName, &configs)
-
-	// Перевыставить уровень логирования на основе конфига
-	//if configs[0].LogLevel == "INFO" {
-	//	debugLevel = "INFO"
-	//	SetLog(log.InfoLevel)
-	//}
-	//log.Infof("Log level: '%s'", debugLevel)
-
-	// Сериализовать в YAML
-
-	config3.LogLevel = "INFO"
-
-	var config2 Backup
-	config2.From = "AAA"
-	config2.To = "BBB"
-	config2.Crypt = true
-	config2.DateTime = true
-	config2.Ssh = "test.com.ru"
-
-	var config5 Backup
-	config5.From = "CCC"
-	config5.To = "DDD"
-	config5.Crypt = false
-	config5.DateTime = true
-	config5.Ssh = "qa.com.ru"
-
-	config3.Backups = append(config3.Backups, config2)
-	config3.Backups = append(config3.Backups, config5)
-
-	d, err := yaml.Marshal(&config3)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("--- config3 dump:\n%s\n\n", string(d))
+	getConfigParameters(configFileName, &config)
 
 }
 
 /* Получить параметры из конфигурационного YAML файла */
-func getConfigParameters(configFileName string, configs *[]Config1) {
+func getConfigParameters(configFileName string, config *Config) {
 
 	// Открыть файл
 	file, err := os.Open(configFileName)
 	if err != nil {
-		log.Fatal("Fail to open configs file '%s': %v", configFileName, err)
+		log.Fatal("Fail to open config file '%s': %v", configFileName, err)
 	}
 
 	// Прочитать весь файл
 	yamlData, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Fatal("Fail to read configs file '%s': %v", configFileName, err)
+		log.Fatal("Fail to read config file '%s': %v", configFileName, err)
 	}
 
 	// Десериализовать
-	err = yaml.Unmarshal(yamlData, &configs)
+	err = yaml.Unmarshal(yamlData, &config)
 	if err != nil {
-		log.Fatal("Fail to unmarshal configs file '%s': %v", configFileName, err)
+		log.Fatal("Fail to unmarshal config file '%s': %v", configFileName, err)
 		os.Exit(1)
 	}
-	log.Infof("=--> configs:\n%v\n\n", *configs)
+
+	// Перевыставить уровень логирования на основе конфига
+	debugLevel := "DEBUG" // "DEBUG", "INFO"
+	if config.LogLevel == "INFO" {
+		debugLevel = "INFO"
+		SetLog(log.InfoLevel)
+	}
+	log.Debugf("Log level: '%s'", debugLevel)
+
+	log.Debugf("=--> config:\n%v\n\n", *config)
 
 	err = file.Close()
 	if err != nil {
-		log.Errorf("Fail to close configs file '%s': %v", configFileName, err)
+		log.Errorf("Fail to close config file '%s': %v", configFileName, err)
 	}
 }
 
